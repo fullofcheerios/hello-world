cd43420291d86899b9ed6cd6f5f3fb9628401c8d
fullofcheerios <33432212+fullofcheerios@users.noreply.github.com>
2018-02-12 23:02:20 -0500

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#<include stdio.h> //directive to preprocessor; standard input/output header
+
+int main(void) //included in every C program;a function; returns an integer
+{
+    printf("Hello world!");  //print statemnt
+    return 0;
+}
